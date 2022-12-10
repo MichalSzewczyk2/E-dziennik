2a28e6f455bb4cc89e55fedacd1dd53d619d0b94
Maniek1337 <55005065+Maniek1337@users.noreply.github.com>
2022-12-11 00:05:27 +0100

    Add project structure

--- src/main/java/com/example/chatui/LogIn.java
+++ src/main/java/com/example/chatui/LogIn.java
@@ -3,7 +3,6 @@ package com.example.chatui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
---
+++ src/main/java/users/User.java
@@ -0,0 +1,14 @@
+package users;
+
+public class User {
+
+    private int userId;
+    private String login;
+    private String password;
+    private String name;
+    private String surname;
+    private String mail;
+    private int phone_number;
+    private Position position;
+
+}
